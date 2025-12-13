83c98b96f21fdad76ad1d64e58c3a16162e3fb47
Hamza <muhammadhamzam1486@gmail.com>
2025-12-13 20:30:08 +0500

    fix: update batch size calculation to use frames per second

--- core/process_video.py
+++ core/process_video.py
@@ -81,8 +81,7 @@ def get_detections_from_video(
     fps = cap.get(cv2.CAP_PROP_FPS)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # TODO: fix batch size
-    batch_size = 500
+    batch_size = fps
 
     print(f"[INFO]: number of batches: {ceil(total_frames / batch_size)}")
     frames = []
