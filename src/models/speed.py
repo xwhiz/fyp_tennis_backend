e997952838e74828750243cb516c2da4f9e17739
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2026-02-15 06:11:31 +0500

    feat: moved to alembic

--- src/models/speed.py
+++ src/models/speed.py
@@ -7,7 +7,7 @@ from src.db.base import Base, TimestampMixin
 class Speed(Base, TimestampMixin):
     """Model for storing speed data."""
     
-    __tablename__ = "speed"
+    __tablename__ = "speeds"
     
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     task_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
