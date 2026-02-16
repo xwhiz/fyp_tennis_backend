afc7767c8ce81a769ea142e8f43163aca4802909
Ahmed-Mohiuddin-Shah <ahmed.mohiuddin.cds@gmail.com>
2026-02-16 14:35:19 +0500

    chore: change entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,5 @@ while ! nc -z postgres 5432; do
   sleep 1
 done
 
-echo "Running migrations..."
-uv run alembic upgrade head
-
-echo "Starting FastAPI..."
-uv run uvicorn src.main:app --host 0.0.0.0 --port 7000
+echo "Postgres is up - executing command"
+exec "$@"
